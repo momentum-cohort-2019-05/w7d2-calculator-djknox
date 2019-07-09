c1c63ab7ec2140885017bcf9ca2a0252e9690681
DJ Knox <dhknox@gmail.com>
2019-07-09 15:35:22 -0400

    add basic input + evaluation validation

--- main.js
+++ main.js
@@ -1,23 +1,35 @@
+const VALID_INPUT = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '+', '-', '/', '*'];
 const display = document.querySelector('#calculator-display');
+
+// add click listeners to every number button with .calculator-button-item class
+// when clicked, they will add their innerText to #calculator-display's innerText
+const buttons = document.querySelectorAll(['.calculator-button-number', '.calculator-button-operator']);
+for (let button of buttons) {
+    button.addEventListener('click', function () {
+        if (VALID_INPUT.includes(button.innerText)) {
+            display.innerText += button.innerText;
+        }
+        else {
+            window.alert("That wasn't valid input!");
+        }
+    });
+}
+
+// add event listener to evaluate the expression within calculator-display's innerText
+// and replace calculator-display's innerText with the result
+const equalsButton = document.querySelector('#calculator-equals');
+equalsButton.addEventListener('click', function () {
+    try {
+        display.innerText = math.evaluate(display.innerText);
+    }
+    catch (error) {
+        window.alert("That wasn't a valid expression!");
+        display.innerText = '';
+    }
+});
 
 // add event listener to clear display when 'C' button is clicked
 const clearButton = document.querySelector('#calculator-clear');
 clearButton.addEventListener('click', function () {
     display.innerText = '';
 });
-
-// add event listener to evaluate the expression within calculator-display's innerText
-// and replace calculator-display's innerText with the result
-const equalsButton = document.querySelector('#calculator-equals');
-equalsButton.addEventListener('click', function () {
-    display.innerText = math.evaluate(display.innerText);
-});
-
-// add click listeners to every button with .calculator-button-item class
-// when clicked, they will add their innerText to #calculator-display's innerText
-const buttons = document.querySelectorAll('.calculator-button-item');
-for (let button of buttons) {
-    button.addEventListener('click', function () {
-        display.innerText += button.innerText;
-    });
-}

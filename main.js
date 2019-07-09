da3c349de18e04049a1e63e303042120bec2223e
DJ Knox <dhknox@gmail.com>
2019-07-09 16:09:41 -0400

    add event listener for mouse hovering over buttons

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const VALID_INPUT = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', '+', '-', '/', '*'];
 const display = document.querySelector('#calculator-display');
 
-// add click listeners to every number button with .calculator-button-item class
-// when clicked, they will add their innerText to #calculator-display's innerText
+// add click listeners to every button with
+// .calculator-button-item and .calculator-button-operator classes
 const buttons = document.querySelectorAll(['.calculator-button-number', '.calculator-button-operator']);
 for (let button of buttons) {
+    // when clicked, they will add their innerText to #calculator-display's innerText
     button.addEventListener('click', function () {
         if (VALID_INPUT.includes(button.innerText)) {
             display.innerText += button.innerText;
@@ -12,6 +13,13 @@ for (let button of buttons) {
         else {
             window.alert("That wasn't valid input!");
         }
+    });
+    // change color when mouse hovers
+    button.addEventListener('mouseenter', function () {
+        button.style.color = "#32DE8A";
+    });
+    button.addEventListener('mouseleave', function () {
+        button.style.color = '';
     });
 }
 
